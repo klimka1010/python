b40fe2deb0207730290d926af117152e3c010414
klimka1010 <klimka1010@gmail.com>
2023-02-09 16:12:44 +0400

    добавил решение второго задания

--- ТЕОРИЯ/Курс Родя/функции, поиск делителей, mostcommon, map.py
+++ ТЕОРИЯ/Курс Родя/функции, поиск делителей, mostcommon, map.py
@@ -1,7 +1,22 @@
 # функции
 def f(x):
     return x ** 2
+
+
 print(f(4))  # 4 ** 2 = 16
 
 # поиск делителей числа
-set()    {}     # массив без повторений (множество)
+D = set()
+{}  # массив без повторений (множество)
+x = 10
+for d in range(1, int(x ** 0.5) + 1):
+    if x % d == 0:
+        D.add(d)
+        D.add(x // d)
+
+# нахождение простого числа
+def isPrime(x):
+    for d in range(2, int(x ** 0.5) + 1):
+        if x%d==0:
+            return False
+        return True
