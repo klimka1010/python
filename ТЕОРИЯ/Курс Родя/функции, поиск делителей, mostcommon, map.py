f14f9562b8f9199740fb745e0b1f45aa2f37c60b
klimka1010 <klimka1010@gmail.com>
2023-02-09 16:45:03 +0400

    fresh update

--- ТЕОРИЯ/Курс Родя/функции, поиск делителей, mostcommon, map.py
+++ ТЕОРИЯ/Курс Родя/функции, поиск делителей, mostcommon, map.py
@@ -1,22 +1,49 @@
-# функции
+# ФУНКЦИЯ
 def f(x):
     return x ** 2
 
-
 print(f(4))  # 4 ** 2 = 16
 
-# поиск делителей числа
-D = set()
-{}  # массив без повторений (множество)
+
+# ПОИСК ДЕЛИТЕЛЕЙ ЧИСЛА
+D = set()  # {} - массив без повторений (множество)
 x = 10
 for d in range(1, int(x ** 0.5) + 1):
     if x % d == 0:
         D.add(d)
         D.add(x // d)
 
-# нахождение простого числа
+
+# НАХОЖДЕНИЕ ПРОСТОГО ЧИСЛА
 def isPrime(x):
     for d in range(2, int(x ** 0.5) + 1):
-        if x%d==0:
+        if x % d == 0:
             return False
         return True
+
+print(isPrime(7))   # True  (1)
+print(isPrime(14))  # False (0)
+
+
+# СКОЛЬКО РАЗ ВСТРЕЧАЮТСЯ СИМВОЛЫ // САМОЕ "ПОПУЛЯРНОЕ" ЧИСЛО
+import collections
+
+s = '12345'
+print(collections.Counter(s).most_common())
+
+
+# ПРОВЕРКА МАССИВВА НА ПРОСТЫЕ ЧИСЛА
+a = [1, 2, 3, 4, 5]
+b = list(map(isPrime, a))
+print(b.count(True))
+
+
+# ДРОБЛЕНИЕ ЧИСЛА В МАССИВ
+a = 1010
+x = list(map(int, str(a)))
+print(x)
+
+print(sorted(x))
+print(sorted(x, reverse=True))
+
+
