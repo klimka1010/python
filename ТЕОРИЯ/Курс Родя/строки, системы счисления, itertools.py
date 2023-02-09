e911d54b4828e78780ba5ddf5d0a2e587b6f173e
klimka1010 <klimka1010@gmail.com>
2023-02-09 18:53:33 +0400

    fresh update

--- ТЕОРИЯ/Курс Родя/строки, системы счисления, itertools.py
+++ ТЕОРИЯ/Курс Родя/строки, системы счисления, itertools.py
@@ -5,8 +5,15 @@ x8 = oct(x)[2:]
 x16 = hex(x)[2:]
 print(x, x2, x8, x16)
 
-# ###
+# перевод в двугую систему счисления
+s = 5 ** 2019 - 5 ** 1019 + 25 ** 600 - 125
+k = 0
+t = ''
 
+while s > 0:
+    t += (str(s % 5))
+    s = s // 5
+print(t.count('4'))
 
 # itertools
 import itertools
